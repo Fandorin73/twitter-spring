57d1ec1800a1b2f030354e9740c4ebc21f0488c2
Fandorin73 <monten49@gmail.com>
2022-05-25 16:26:17 +0300

    Initial application framework

---
+++ src/main/java/com/home/twitterspring/TwitterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.home.twitterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterSpringApplication.class, args);
+    }
+
+}
